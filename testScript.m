4bdb2ea2227cd47aeee6ed3211ff9346c5195f0c
MarionTormento <marion.tormento@eleves.enpc.fr>
2018-02-16 19:12:05 +0000

    correction of a few mistakes, make it globally work, still an error remaining

--- testScript.m
+++ testScript.m
@@ -17,7 +17,7 @@ visBags(bags, replacement, infoGain);
 
 %% Split Function
 param.rho = 3;
-param.numlevels = 3;
+param.numlevels = 4;
 
 %% Recursive test
 
@@ -43,31 +43,36 @@ for k = 1:n
 
         %for each child we decide on an optimum split function
         for i = 1:length(children)
-            
+%%% SECURITY IS CLASS FULL %%%%%%
             rootNode = children{i};
-            [childrenNew, infoGain] = optimalNodeSplit(param, rootNode);
-            visNodes(childrenNew, replacement, infoGain);
-% ADD SECURITY IF ONLY ONE CHILDREN
-            % Complete the tree
-            tree{k}{j,2*i-1} = childrenNew{1};    
-            tree{k}{j,2*i} = childrenNew{2};
-            % Collect a next children array for next branch
-            childrenNext{2*i-1} = childrenNew{1};
-            childrenNext{2*i} = childrenNew{2};  
-            
-            clear rootNode
-            clear childrenNew
-            clear infoGain
-            pause         
-        end
+            isLeaf = leafTest(rootNode); 
+            if isLeaf < 2
+                [childrenNew, infoGain] = optimalNodeSplit(param, rootNode);
+                visNodes(childrenNew, replacement, infoGain);
+                % Complete the tree
+                tree{k}{j,2*i-1} = childrenNew{1};  
+                tree{k}{j,2*i} = childrenNew{2};
+                % Collect a next children array for next branch
+                childrenNext{2*i-1} = childrenNew{1};
+                childrenNext{2*i} = childrenNew{2};  
 
+                clear rootNode
+                clear childrenNew
+                clear infoGain
+                pause         
+            end
+        end
+        
+        clear children
         %redefine our new generation of children as our current children for
         %the next layer of the tree
         children = childrenNext;
+        clear ChildrenNext
 
     end
 
 end
+
 
 function [bags] = bagging(n, s, data_train, replacement)
     
@@ -89,212 +94,12 @@ function [bags] = bagging(n, s, data_train, replacement)
     end
 end
 
-function visNodes(inputs, replacement, infoGain)
-
-    %error debugging, ensuring the input cell is a structure if one of the children in empty
-    if ~iscell(inputs)
-        inputscell{1}(:,:) = inputs;
-        inputscell{2} =[];
-        clear children
-        inputs = inputscell;
-    end
-
-    % Plot the position of the toy present in each bag
-    figure()
-
-    if infoGain(1,1) == 'X'
-       threshold_y = -1:0.1:1;
-       threshold_x = infoGain(1,2)*ones(1,length(threshold_y));
-    else
-       threshold_x = -1:0.1:1; 
-       threshold_y = infoGain(1,1).*threshold_x+infoGain(1,2);
-    end
-
-    for i = 1:length(inputs)
-        subplot(2,2,1)
-        for j = 1:length(inputs{i})
-            if inputs{i}(j,3) == 1
-                plot(inputs{i}(j,1),inputs{i}(j,2),'or')
-                hold on
-            elseif inputs{i}(j,3) == 2
-                plot(inputs{i}(j,1),inputs{i}(j,2),'+b')
-                hold on
-            elseif inputs{i}(j,3) == 3
-                plot(inputs{i}(j,1),inputs{i}(j,2),'*g')
-                hold on
-            end
-        end
-        if ~isempty(infoGain)
-            if replacement == 0
-                title({['Parent and threshold without replacement,'];['info gain = ' num2str(infoGain(1,3))]})
-            elseif replacement == 1
-                title({['Parent and threshold with replacement,'];['info gain = ' num2str(infoGain(1,3))]})
-            end
-        else
-            if replacement == 0
-                title(['Parent and threshold without replacement'])
-            elseif replacement == 1
-                title(['Parent and threshold with replacement'])
-            end
-        end
-        xlabel('x co-ordinate')
-        ylabel('y co-ordinate')
-        plot(threshold_x,threshold_y)
-        axis([-1 1 -1 1])
-        grid on
-    end
-
-    % Plot the histogram of the toy class repartition in each bag
-    %figure
-    for i = 1:length(inputs)
-        subplot(2,2,i+2)
-        if ~isempty(inputs{i}) %if there are no points in the child, don't plot histogram or errors
-            histogram(inputs{i}(:,3))
-        end
-        xlabel('Category')
-        ylabel('# of Occurences')
-        if ~isempty(infoGain)
-                title({['Child ' num2str(i) ','];['info gain = ' num2str(infoGain(1,3))]})
-        else
-                title(['Child ' num2str(i) '.'])
-        end
-        grid on
-    end
-
-end
-
-function [childrenBest, infoGainBest] = axisNodeSplit(minX, maxX, rootNode, rho) % Compute the best 'x=...' split node for the bag
-    infoGainBest = [0,0,0];
-    childrenBest = [];
-    randomSamp = randperm(round((maxX-minX)/0.1),rho);
-    % Axis Split Function for x=i
-    for i = 1:rho
-        linSplitThreshold = ['X', randomSamp(i)*0.1];
-        [children, infoGain] = childrenAndInfo(rootNode, linSplitThreshold);
-        if infoGain > infoGainBest(3)
-            infoGainBest = [NaN, randomSamp(i)*0.1, infoGain];
-            childrenBest = children;
-        end
-    end
-    if isempty(childrenBest) 
-        childrenBest = rootNode;
-    end
-    clear children
-    clear infoGain
-    clear linSplitThreshold
-end
-
-function [childrenBest, infoGainBest] = linearNodeSplit(minYInt, maxYInt, rootNode, rho) % Compute the best "y=mx+p" split node for the bag
-    
-    %n = 1;
-    infoGainBest = [0,0,0];
-    childrenBest = [];
-    randomSampYInt = randperm(round((maxYInt-minYInt)/0.1),rho);
-    %given an y intercept, calculate good max and min gradients
-    if rootNode(rootNode(:,2) == min(rootNode(:,2)),1) < 0
-        maxGrad = (randomSampYInt - min(rootNode(:,2)))./(-rootNode(rootNode(:,2) == min(rootNode(:,2)),1));
-        minGrad = (randomSampYInt - max(rootNode(:,2)))./(-rootNode(rootNode(:,2) == max(rootNode(:,2)),1));
-    else
-        minGrad = (randomSampYInt - min(rootNode(:,2)))./(-rootNode(rootNode(:,2) == min(rootNode(:,2)),1));
-        maxGrad = (randomSampYInt - max(rootNode(:,2)))./(-rootNode(rootNode(:,2) == max(rootNode(:,2)),1));
-    end
-    %Linear Split Function y = m*x+p
-    for m = 1:rho
-        %for each yint, calc 3 random grads between that points man and min
-        %grad
-        randomSampGrad(m,:) = minGrad/0.1 + randperm(round((abs(maxGrad(m))+abs(minGrad(m)))/0.1),rho);
-        for p = 1:rho
-            linSplitThreshold = [randomSampGrad(m,p)*0.1, randomSampYInt(p)*0.1];
-            [children, infoGain] = childrenAndInfo(rootNode, linSplitThreshold);
-            if infoGain > infoGainBest(3)
-                 infoGainBest = [randomSampGrad(m,p)*0.1, randomSampYInt(p)*0.1,infoGain];
-                 childrenBest = children;
-            end
-        end
-    end
-    if isempty(childrenBest) 
-        childrenBest = rootNode;
-    end
-end
-
-function [childrenBest, infoGainBest] = optimalNodeSplit(param, rootNode) % compute the optimal split node between axis and linear
-    
-    rho = param.rho;
-    X = [min(rootNode(:,1)), max(rootNode(:,1))];
-    YInt = [min(rootNode(:,2)), max(rootNode(:,2))];
-    
-    [axisCh, axisInfo] = axisNodeSplit(X(1), X(2), rootNode, rho);
-    [linearCh, linearInfo] = linearNodeSplit(YInt(1), YInt(2), rootNode, rho);
-    
-    [maxInfo idxInfo] = max([axisInfo(1,3), linearInfo(1,3)]); %if idxInfo return 1 => Axis, 2 => linear
-    sprintf('Length axis : %s', length(axisCh), '\n Length linear: %d', length(linearCh), '\n Max Info: ', num2str(maxInfo));
-    if idxInfo == 1 
-            childrenBest = axisCh;
-            infoGainBest = axisInfo;
-    elseif idxInfo == 2
-            childrenBest = linearCh;
-            infoGainBest = linearInfo;
-    end
-end
-    
-function [outputnodes, infoGain] = childrenAndInfo(inputnode, linSplitThreshold)
-
-m = linSplitThreshold(1);
-p = linSplitThreshold(2);
-
-if m == NaN % Axis aligned x = p
-    index = (inputnode(:,1) - p)>0 ;
-else % Axis aligned y = p and linear function y = m*x+p
-    index = (inputnode(:,2) - p - m*inputnode(:,1))>0 ;
-end
-outputnodes{1} = inputnode(index == 0,:);
-outputnodes{2} = inputnode(index == 1,:);
-
-clear m
-clear p
-clear index
-
-infoGain = computeInfo(inputnode, outputnodes);
-
-end % return the ouput nodes for desired threhold, and the corresponding info gain
-
-function info = computeInfo(inputnode, outputnodes)
-%% Entropy before
-for i=1:3
-    if ~isempty(inputnode((inputnode(:,3) == i) == 1,:))
-        prob(i,1) = length(inputnode((inputnode(:,3) == i) == 1,:))/length(inputnode(:,1));
-    end
-end
-entBefore = sum(-prob.*log(prob),1);
-clear prob
-
-%% Entropy After
-for j = 1:2
-    for i = 1:3
-        if ~isempty(outputnodes{j}((outputnodes{j}(:,3) == i) == 1,:))
-            prob(i,j) = length(outputnodes{j}((outputnodes{j}(:,3) == i) == 1,:))/length(outputnodes{j}(:,1));
-        end
-    end
-end
-prob_parent = [length(outputnodes{1}(:,1))/length(inputnode(:,1)) length(outputnodes{2}(:,1))/length(inputnode(:,1))];
-entAfter = sum(sum(-prob.*log(prob),1).*prob_parent,2); % Entropy Before
-clear prob_parent
-clear prob
-
-%% Information gain
-info = entBefore - entAfter;
-clear entAfter
-clear entBefore
-end %compute the info gain
-
-
-
 function visBags(inputs, replacement, infoGain)
 % Plot the position of the toy present in each bag
 figure
 for i = 1:length(inputs)
     subplot(2,2,i)
-    for j = 1:length(inputs{i})
+    for j = 1:size(inputs{i},1)
         if inputs{i}(j,3) == 1
             plot(inputs{i}(j,1),inputs{i}(j,2),'or')
             hold on
@@ -323,4 +128,222 @@ for i = 1:length(inputs)
     end
     grid on
 end
+end
+
+function visNodes(inputs, replacement, infoGain)
+
+    %error debugging, ensuring the input cell is a structure if one of the children in empty
+    if ~iscell(inputs)
+        inputscell{1}(:,:) = inputs;
+        inputscell{2} =[];
+        clear children
+        inputs = inputscell;
+    end
+
+    % Plot the position of the toy present in each bag
+    figure()
+
+    if infoGain.x1 == 'X'
+       threshold_y = -1:0.1:1;
+       threshold_x = infoGain.x2*ones(1,length(threshold_y));
+    else
+       threshold_x = -1:0.1:1; 
+       threshold_y = infoGain.x1.*threshold_x+infoGain.x2;
+    end
+
+    for i = 1:length(inputs)
+        subplot(2,2,1)
+        for j = 1:size(inputs{i},1)
+            if inputs{i}(j,3) == 1
+                plot(inputs{i}(j,1),inputs{i}(j,2),'or')
+                hold on
+            elseif inputs{i}(j,3) == 2
+                plot(inputs{i}(j,1),inputs{i}(j,2),'+b')
+                hold on
+            elseif inputs{i}(j,3) == 3
+                plot(inputs{i}(j,1),inputs{i}(j,2),'*g')
+                hold on
+            end
+        end
+        if ~isempty(infoGain)
+            if replacement == 0
+                title({['Parent and threshold without replacement,'];['info gain = ' num2str(infoGain.Gain)]})
+            elseif replacement == 1
+                title({['Parent and threshold with replacement,'];['info gain = ' num2str(infoGain.Gain)]})
+            end
+        else
+            if replacement == 0
+                title(['Parent and threshold without replacement'])
+            elseif replacement == 1
+                title(['Parent and threshold with replacement'])
+            end
+        end
+        xlabel('x co-ordinate')
+        ylabel('y co-ordinate')
+        plot(threshold_x,threshold_y)
+        axis([-1 1 -1 1])
+        grid on
+    end
+
+    % Plot the histogram of the toy class repartition in each bag
+    %figure
+    for i = 1:length(inputs)
+        subplot(2,2,i+2)
+        if ~isempty(inputs{i}) %if there are no points in the child, don't plot histogram or errors
+            histogram(inputs{i}(:,3))
+        end
+        xlabel('Category')
+        ylabel('# of Occurences')
+        if ~isempty(infoGain)
+                title({['Child ' num2str(i) ','];['info gain = ' num2str(infoGain.Gain)]})
+        else
+                title(['Child ' num2str(i) '.'])
+        end
+        grid on
+    end
+
+end
+
+function [childrenBest, infoGainBest] = axisNodeSplit(minX, maxX, rootNode, rho) % Compute the best 'x=...' split node for the bag
+    infoGainBest.x1 = 'X';
+    infoGainBest.x2 = 0;
+    infoGainBest.Gain = 0;
+    childrenBest = [];
+    randomSamp = randperm(round((maxX-minX)/0.01),rho);
+    % Axis Split Function for x=i
+    linSplitThreshold.x1 = 'X';
+    for i = 1:rho
+        threshold = minX + 0.01*randomSamp(i);
+        linSplitThreshold.x2 = threshold;
+        [children, infoGain] = childrenAndInfo(rootNode, linSplitThreshold);
+        if infoGain > infoGainBest.Gain
+            infoGainBest.x2 = threshold;
+            infoGainBest.Gain = infoGain;
+            childrenBest = children;
+%         elseif i == rho && sum(infoGainBest) == 0
+%             infoGainBest = ['X', threshold, infoGain];
+%             childrenBest = children;         
+        end
+    end
+
+    clear children
+    clear infoGain
+    clear linSplitThreshold
+end
+
+function [childrenBest, infoGainBest] = linearNodeSplit(minYInt, maxYInt, rootNode, rho) % Compute the best "y=mx+p" split node for the bag
+    
+    infoGainBest.x1 = 'X';
+    infoGainBest.x2 = 0;
+    infoGainBest.Gain = 0;
+    childrenBest = [];
+    randomSampYInt = randperm(round((maxYInt-minYInt)/0.1),rho);
+    %given an y intercept, calculate good max and min gradients
+    if rootNode(rootNode(:,2) == min(rootNode(:,2)),1) < 0
+        maxGrad = (randomSampYInt - min(rootNode(:,2)))./(-rootNode(rootNode(:,2) == min(rootNode(:,2)),1));
+        minGrad = (randomSampYInt - max(rootNode(:,2)))./(-rootNode(rootNode(:,2) == max(rootNode(:,2)),1));
+    else
+        minGrad = (randomSampYInt - min(rootNode(:,2)))./(-rootNode(rootNode(:,2) == min(rootNode(:,2)),1));
+        maxGrad = (randomSampYInt - max(rootNode(:,2)))./(-rootNode(rootNode(:,2) == max(rootNode(:,2)),1));
+    end
+    %Linear Split Function y = m*x+p
+    for m = 1:rho
+        %for each yint, calc 3 random grads between that points man and min
+        %grad
+        randomSampGrad(m,:) = minGrad/0.1 + randperm(round((abs(maxGrad(m))+abs(minGrad(m)))/0.1),rho);
+        for p = 1:rho
+            linSplitThreshold.x1 = randomSampGrad(m,p)*0.1;
+            linSplitThreshold.x2 = randomSampYInt(p)*0.1;
+            [children, infoGain] = childrenAndInfo(rootNode, linSplitThreshold);
+            if infoGain > infoGainBest.Gain
+                 infoGainBest.x1 = randomSampGrad(m,p)*0.1;
+                 infoGainBest.x2 = randomSampYInt(p)*0.1;
+                 infoGainBest.Gain = infoGain;
+                 childrenBest = children;
+%             elseif p == rho && sum(infoGainBest) == 0
+%                  infoGainBest = [randomSampGrad(m,p)*0.1, randomSampYInt(p)*0.1,infoGain];
+%                  childrenBest = children;
+            end
+         end
+    end
+end
+
+function [childrenBest, infoGainBest] = optimalNodeSplit(param, rootNode) % compute the optimal split node between axis and linear
+    
+    rho = param.rho;
+    X = [min(rootNode(:,1)), max(rootNode(:,1))];
+    YInt = [min(rootNode(:,2)), max(rootNode(:,2))];
+    
+    [axisCh, axisInfo] = axisNodeSplit(X(1), X(2), rootNode, rho);
+    [linearCh, linearInfo] = linearNodeSplit(YInt(1), YInt(2), rootNode, rho);
+    
+    [maxInfo idxInfo] = max([axisInfo.Gain, linearInfo.Gain]); %if idxInfo return 1 => Axis, 2 => linear
+    if idxInfo == 1 
+            childrenBest = axisCh;
+            infoGainBest = axisInfo;
+    elseif idxInfo == 2
+            childrenBest = linearCh;
+            infoGainBest = linearInfo;
+    end
+end
+    
+function [outputnodes, infoGain] = childrenAndInfo(inputnode, linSplitThreshold)
+
+m = linSplitThreshold.x1;
+p = linSplitThreshold.x2;
+
+if m == 'X' % Axis aligned x = p
+    index = (inputnode(:,1) - p)>0 ;
+else % Axis aligned y = p and linear function y = m*x+p
+    index = (inputnode(:,2) - p - m*inputnode(:,1))>0 ;
+end
+outputnodes{1} = inputnode(index == 0,:);
+outputnodes{2} = inputnode(index == 1,:);
+
+clear m
+clear p
+clear index
+
+infoGain = computeInfo(inputnode, outputnodes);
+
+end % return the ouput nodes for desired threhold, and the corresponding info gain
+
+function info = computeInfo(inputnode, outputnodes)
+%% Entropy before
+for i=1:3
+    if ~isempty(inputnode((inputnode(:,3) == i) == 1,:))
+        prob(i,1) = size(inputnode((inputnode(:,3) == i) == 1,:),1)/size(inputnode(:,1),1);
+    else
+        prob(i,1) = 1; 
+    end
+end
+entBefore = sum(-prob.*log(prob),1);
+clear prob
+
+%% Entropy After
+for j = 1:2
+    for i = 1:3
+        if ~isempty(outputnodes{j}((outputnodes{j}(:,3) == i) == 1,:))
+            prob(i,j) = size(outputnodes{j}((outputnodes{j}(:,3) == i) == 1,:),1)/size(outputnodes{j}(:,1),1);
+        else
+           prob(i,j) = 1; 
+        end
+    end
+end
+prob_parent = [size(outputnodes{1}(:,1),1)/size(inputnode(:,1),1) size(outputnodes{2}(:,1),1)/size(inputnode(:,1),1)];
+entAfter = sum(sum(-prob.*log(prob),1).*prob_parent,2); % Entropy Before
+clear prob_parent
+clear prob
+
+%% Information gain
+info = entBefore - entAfter;
+clear entAfter
+clear entBefore
+end %compute the info gai
+
+function bin = leafTest(rootNode)
+    for i = 1:3
+        bin(i,1) = isempty(rootNode((rootNode(:,3) == i) == 1,:))
+    end
+    bin = sum(bin);
 end
