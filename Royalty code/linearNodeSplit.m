3ac799817c472207fa8318d3151c4ad531116667
Edward McLaughlin <em215@imperial.ac.uk>
2018-02-22 08:00:11 +0000

    Check logBook.txt

--- Royalty code/linearNodeSplit.m
+++ Royalty code/linearNodeSplit.m
@@ -7,19 +7,25 @@ function [childrenBest, infoGainBest] = linearNodeSplit(minYInt, maxYInt, rootNo
     childrenBest = [];
     
     randomSampYInt = randperm(round(abs(maxYInt - minYInt)/0.001),numfunct);
-    %given an y intercept, calculate good max and min gradients
-    if rootNode(rootNode(:,2) == min(rootNode(:,2)),1) < 0
-        maxGrad = (randomSampYInt - min(rootNode(:,2))/0.001)*0.001./(-rootNode(rootNode(:,2) == min(rootNode(:,2)),1));
-        minGrad = (randomSampYInt - max(rootNode(:,2))/0.001)*0.001./(-rootNode(rootNode(:,2) == max(rootNode(:,2)),1));
-    else 
-        minGrad = (randomSampYInt - min(rootNode(:,2))/0.001)*0.001./(-rootNode(rootNode(:,2) == min(rootNode(:,2)),1));
-        maxGrad = (randomSampYInt - max(rootNode(:,2))/0.001)*0.001./(-rootNode(rootNode(:,2) == max(rootNode(:,2)),1));
-    end
+    %given a y intercept, calculate the gradients of the lines to the min
+    %and max data points. These gradients are calculated for the 3
+    %(numfunct) y intercepts which we try
+    Grad1 = (randomSampYInt - min(rootNode(:,2))/0.001)*0.001./(-rootNode(rootNode(:,2) == min(rootNode(:,2)),1));
+    Grad2 = (randomSampYInt - max(rootNode(:,2))/0.001)*0.001./(-rootNode(rootNode(:,2) == max(rootNode(:,2)),1));
     %Linear Split Function y = m*x+p
     for m = 1:numfunct
-        %for each yint, calc 3 random grads between that points man and min
-        %grad
-        randomSampGrad(m,:) = minGrad/0.001 + randperm(round((abs(maxGrad(m)+minGrad(m))/0.001)),numfunct);
+        %for each yint, calculagte which gradient is larger
+    if Grad1(m) > Grad2(m)
+        maxGrad = Grad1(m);
+        minGrad = Grad2(m);
+    else 
+        maxGrad = Grad2(m);
+        minGrad = Grad1(m);
+    end
+    % calculate a random gradient in between the min and max possible
+        randomSampGrad(m,:) = minGrad/0.001 + randperm(round((maxGrad-minGrad)/0.001),numfunct);
+        %perform linear split based on this y int and its gradient randomly
+        %generated
         for p = 1:numfunct
             linSplitThreshold.x1 = randomSampGrad(m,p)*0.001;
             linSplitThreshold.x2 = randomSampYInt(p)*0.001;
