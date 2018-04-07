184e464d98e81eec297db55fdd36459caefdf828
Edward McLaughlin <em215@imperial.ac.uk>
2018-04-07 21:00:09 +0100

    blah

--- Coursework 2/Code/MainCode.py
+++ Coursework 2/Code/MainCode.py
@@ -6,106 +6,43 @@ import time
 
 def getImageIntensity(image):
 
-	print('Starting intensity find')
-	t = time.time()
-
 	# Load an color image in grayscale
 	img = cv2.imread('Photos/' + image, 0)
 	width, height = img.shape
 
 	return img
 
-def findIntensity(image, inputSmoothing, plot1, plot2):
-
-	print('Starting intensity find')
-	t = time.time()
-
-	# Load an color image in grayscale
-	img = cv2.imread('Photos/' + image, 0)
-	width, height = img.shape
-
-	# Plot image and see its full histogram
-	if plot1>0:
-		fig1 = plt.figure()
-		plt.subplot(221), plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-		# plt.subplot(212), plt.hist(img.ravel(),256,[0,256]);
-
-	# Define length scale over which with intensity is averaged
-	smoothing = int(inputSmoothing)
-	HSplits = int(height/inputSmoothing)
-	WSplits = int(width/inputSmoothing)
-
-	# Initialise stuff
-	descriptor_mean_inten = np.zeros([WSplits, HSplits])
-
-	for i in range(0,WSplits):
-		for ii in range(0,HSplits):
-			# create a mask
-			mask = np.zeros(img.shape[:2], np.uint8)
-			mask[int((i)*width/WSplits):int((i+1)*width/WSplits), int((ii)*height/HSplits):int((ii+1)*height/HSplits)] = 255
-			masked_img = cv2.bitwise_and(img,img,mask = mask)
-			hist_mask = cv2.calcHist([img],[0],mask,[256],[0,256])
-			# Plot each section of the image and its histogram
-			if plot2>0:
-				figure = plt.figure()
-				plt.subplot(211), plt.imshow(masked_img, 'gray')
-				plt.subplot(212), plt.plot(hist_mask)
-				plt.xlim([0,256])
-			sumInten = 0
-			sumHist = 1
-			# Calculate the mean intensity of the pixels in this section of the image
-			for j in range(0,255):
-				sumInten += j*hist_mask[j]
-				sumHist += hist_mask[j]
-			meanInten = int(sumInten)/int(sumHist)
-			descriptor_mean_inten[i][ii] = int(meanInten)
-
-			del mask, masked_img, hist_mask, sumHist, meanInten, sumInten
-
-	# descriptor_mean_intenArray = np.asarray(descriptor_mean_inten) 
-	#print(np.asarray(descriptor_mean_inten) is descriptor_mean_inten)
-	print(descriptor_mean_inten)
-	#descriptor_mean_intenArray.reshape((HSplits, WSplits))
-	print(HSplits,WSplits)
-	print(descriptor_mean_inten.shape)
-
-	elapsed = time.time() - t
-	print('Time to find mean intensity = % 1.0fs' % elapsed)
-
-	return descriptor_mean_inten, WSplits, HSplits
-
 def derivatives(intensity, shift):
 
-	print('Starting Derivatives')
-	t = time.time()
+	# Function to calculate the change in intensity in the image in the 
+	# x and y directions across 'shift' number of pixels.
+	# 	INPUTS: Image intensity and the number of pixels to calculate the 
+	# 			derivative across
+	# 	OUTPUT: Derivative of the intensity in the x and y directions (Ix, Iy)
+	# 	PLOT  : The square of the derivatives of the pixels intensity in 
+	# 	the x and y direction (Ixx, Iyy) and the product of Ix and Iy (Ixy)
 
 	# Find Ix and Iy
 	intensity = np.asarray(intensity)
 	intensityShiftX = np.roll(intensity, shift, axis=1)
 	intensityShiftY = np.roll(intensity, shift, axis=0)
 
+	# Find the average change over 'shift' number of pixels
 	Ix = np.subtract(intensityShiftX, intensity) / shift
 	Iy = np.subtract(intensityShiftY, intensity) / shift
+
+	# Find the squares of these
 	Ixx = Ix ** 2
 	Iyy = Iy ** 2
 	Ixy = Ix * Iy
 
-	elapsed = time.time() - t
-	print('Time to find derivatives = % 1.0fs' % elapsed)
-
-	print('Plotting Derivatives')
-	t1 = time.time()
-
+	# Plotting
 	figure = plt.figure()
-	# plt.subplot(222), plt.imshow(intensity, cmap='gray', interpolation='nearest')
 	plt.subplot(231), plt.imshow(Ix, cmap='gray', interpolation='nearest')
 	plt.subplot(232), plt.imshow(Iy, cmap='gray', interpolation='nearest')
 	plt.subplot(233), plt.imshow(Ixx, cmap='gray', interpolation='nearest')
 	plt.subplot(234), plt.imshow(Iyy, cmap='gray', interpolation='nearest')
 	plt.subplot(235), plt.imshow(Ixy, cmap='gray', interpolation='nearest')
-
-	elapsed1 = time.time() - t1
-	print('Time to plot derivatives = % 1.0fs' % elapsed1)
 
 	del intensity
 
@@ -113,6 +50,11 @@ def derivatives(intensity, shift):
 
 def gaussian_window(Ix, Iy, sigma):
 
+	# INPUTS: Derivatives of image intensity in the x and y directions
+	# OUTPUTS: Gaussian filtered intensity derivatives.
+
+	# Apply a gaussian window to the respective components of the 2x2 matrix
+	# containing the derivatives of the image intensity.
 	GIxx = ndimage.gaussian_filter(Ix**2, sigma)
 	GIyy = ndimage.gaussian_filter(Iy**2, sigma)
 	GIxy = ndimage.gaussian_filter(Ix*Iy, sigma)
@@ -121,29 +63,52 @@ def gaussian_window(Ix, Iy, sigma):
 
 def cornerness_funct(GIxx, GIyy, GIxy, alpha):
 
-	print('Starting Cornerness')
-	t = time.time()
+	# Function to calculate the locations of corners and edges in the image
+	
+	# Calculate R
+	R = (GIxx)*(GIyy) - GIxy**2 - alpha*(GIxx + GIyy)**2
+	# Rwidth, Rheight = R.shape
+	# cornerPointsX = np.ones((1))
+	# cornerPointsY = np.ones((1))
 
-	# Ixx = Ix ** 2
-	# Iyy = Iy ** 2
-	# Ixy = Ix * Iy
+	# for i in range(1,Rheight-1):
+	# 	for ii in range(1,Rwidth-1):
+	# 		RBox = np.ones((3, 3))
+	# 		for j in [0, 1, 2]:
+	# 			for jj in [0, 1, 2]:
+	# 				RBox[j][jj] = R[i-1+j][ii-1+jj]
+	# 		maxR = np.where(RBox==np.amax(RBox))
+	# 		# print(RBox)
+	# 		print(maxR)
+	# 		if (maxR[0] & maxR[1]) == 1:
+	# 			print(i)
+	# 			print(ii)
+	# 			np.append(cornerPointsX, ii)
+	# 			np.append(cornerPointsY, i)
+	# 			# np.concatenate([cornerPointsX, ii], axis=0)
+	# 			# np.concatenate([cornerPointsY, i], axis=0)
+	# 		del RBox
 
-	cornerness = (GIxx)*(GIyy) - GIxy**2 - alpha*(GIxx + GIyy)
 
-	elapsed = time.time() - t
-	print('Time to find cornerness = % 1.0fs' % elapsed)
+	thresholdCorner = 0.8*np.amax(R)
+	thresholdEdge = 0.8*np.amin(R)
+	# Based on each pixels value of R, determine if it is a corner or an edge
+	# or neither. 
+	cornerPoints = np.where(R > thresholdCorner)
+	cornerPointsX = cornerPoints[1]
+	cornerPointsY = cornerPoints[0]
+	edgePoints = np.where(R < thresholdEdge)
+	edgePointsX = edgePoints[1]
+	edgePointsY = edgePoints[0]
 
-	print('Plotting Cornerness')
-	t1 = time.time()
-
+	# Plot
 	plt.figure()
-	plt.imshow(cornerness, cmap='gray', interpolation='nearest')
+	plt.imshow(R, cmap='gray', interpolation='nearest')
+	plt.scatter(cornerPointsX, cornerPointsY, color='r', marker='+')
+	# plt.scatter(edgePointsX, edgePointsY, color='g', marker='+')
 	plt.show()
 
-	elapsed1 = time.time() - t1
-	print('Time to plot cornerness = % 1.0fs' % elapsed1)
-
-	return cornerness
+	return R
 
 # ------------------------- Main Script --------------------------------
 
@@ -154,20 +119,20 @@ FD = (['FD1.jpg', 'FD2.jpg', 'FD3.jpg', 'FD4.jpg', 'FD5.jpg', 'FD6.jpg',
 HD = (['3.2_1.jpg', '3.2_2.jpg', '3.2_3.jpg',  '4.0_1.jpg', '4.0_2.jpg',
       '4.0_3.jpg', '5.0_1.jpg', '5.0_2.jpg', '5.0_3.jpg'])
 
+Test_images = (['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg'])
+
 for i in range(0,1):
 
-	intensity = getImageIntensity(FD[i])
+	intensity = getImageIntensity(HD[i])
 
 	Ix, Iy = derivatives(intensity, 15)
 	
 	GIxx, GIyy, GIxy = gaussian_window(Ix, Iy, 3)
 	
-	cornerness = cornerness_funct(GIxx, GIyy, GIxy, 0.06)
+	R = cornerness_funct(GIxx, GIyy, GIxy, 0.05)
+	print(np.amax(R))
+	print(np.amin(R))
 	
-	# sorted = np.sort(cornerness, axis = None)
-	plt.hist(cornerness)
-	plt.show()
-
 # for i in range(0,1):
 
 # 	intensity = getImageIntensity(HD[i])
