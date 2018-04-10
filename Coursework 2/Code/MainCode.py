fa3837b3cba8c3fc50a3aa7db12468c4096352f9
MarionTormento <marion.tormento@eleves.enpc.fr>
2018-04-10 22:25:22 +0100

    harris detector final version?

--- Coursework 2/Code/MainCode.py
+++ Coursework 2/Code/MainCode.py
@@ -10,10 +10,11 @@ def getImageIntensity(image):
 	# Load an color image in grayscale
 	img = cv2.imread('Photos/' + image, 0)
 	
+	# Autonomously find the size of the shift window adapted to the image
 	width, height = img.shape
 	shift_w = int(width/50)
 	shift_h = int(height/50)
-	shift = min(shift_h, shift_w)
+	shift = max(1, min(shift_h, shift_w)) # In case the image is smaller than 50x50, shift = 1
 
 	return img, shift
 
@@ -27,7 +28,6 @@ def derivatives(intensity, shift):
 	# 	PLOT  : The square of the derivatives of the pixels intensity in 
 	# 	the x and y direction (Ixx, Iyy) and the product of Ix and Iy (Ixy)
 	
-	# Method inspired from matlab solution on youtube
 	# Find Ix and Iy
 	intensity = np.asarray(intensity)
 
@@ -38,16 +38,6 @@ def derivatives(intensity, shift):
 
 	Ix = signal.convolve2d(intensity, dx, 'same')
 	Iy = signal.convolve2d(intensity, dy, 'same')
-
-
-	# # Find Ix and Iy
-	# intensity = np.asarray(intensity)
-	# intensityShiftX = np.roll(intensity, shift, axis=1)
-	# intensityShiftY = np.roll(intensity, shift, axis=0)
-
-	# # Find the average change over 'shift' number of pixels
-	# Ix = np.subtract(intensityShiftX, intensity) / shift
-	# Iy = np.subtract(intensityShiftY, intensity) / shift
 
 	# Find the squares of these
 	Ixx = Ix ** 2
@@ -71,19 +61,17 @@ def gaussian_window(Ix, Iy, sigma, shift):
 	# INPUTS: Derivatives of image intensity in the x and y directions
 	# OUTPUTS: Gaussian filtered intensity derivatives.
 
-	# Method inspired from matlab solution on youtube
 	# Define the gaussian window sized by the shift
-	dim = max(1, shift) 
-	m0 = -(dim-1)/2
+	m0 = -(shift-1)/2
 	m = []
 	n = []
-	for i in range(0, dim):
+	for i in range(0, shift):
 		m.append(m0+i)
-	for i in range(0,dim-1):
+	for i in range(0,shift-1):
 		n.append(m0+i)
 	h1, h2 = np.meshgrid(m,n)
 	gauss = np.exp(-(h1 ** 2 + h2 ** 2)/(2*sigma**2))
-	sumtot = np.sum(gauss);
+	sumtot = np.sum(gauss)
 	gauss = gauss/sumtot
 
 	# Convolution of the Intensity matrix and the gaussian window
@@ -91,100 +79,80 @@ def gaussian_window(Ix, Iy, sigma, shift):
 	GIyy = signal.convolve2d(Iy ** 2, gauss, 'same')
 	GIxy = signal.convolve2d(Ix * Iy, gauss, 'same')
 
-	# # Apply a gaussian window to the respective components of the 2x2 matrix
-	# # containing the derivatives of the image intensity.
-	# GIxx = ndimage.gaussian_filter(Ix**2, sigma)
-	# GIyy = ndimage.gaussian_filter(Iy**2, sigma)
-	# GIxy = ndimage.gaussian_filter(Ix*Iy, sigma)
-
 	return GIxx, GIyy, GIxy
 
-def cornerness_funct(GIxx, GIyy, GIxy, alpha):
+def cornerness_funct(image, GIxx, GIyy, GIxy, alpha):
 
 	# Function to calculate the locations of corners and edges in the image
 	
 	# Calculate R
 	R = (GIxx)*(GIyy) - GIxy**2 - alpha*(GIxx + GIyy)**2
-	# Rwidth, Rheight = R.shape
-	# cornerPointsX = np.ones((1))
-	# cornerPointsY = np.ones((1))
-
-	# for i in range(1,Rheight-1):
-	# 	for ii in range(1,Rwidth-1):
-	# 		RBox = np.ones((3, 3))
-	# 		for j in [0, 1, 2]:
-	# 			for jj in [0, 1, 2]:
-	# 				RBox[j][jj] = R[i-1+j][ii-1+jj]
-	# 		maxR = np.where(RBox==np.amax(RBox))
-	# 		# print(RBox)
-	# 		print(maxR)
-	# 		if (maxR[0] & maxR[1]) == 1:
-	# 			print(i)
-	# 			print(ii)
-	# 			np.append(cornerPointsX, ii)
-	# 			np.append(cornerPointsY, i)
-	# 			# np.concatenate([cornerPointsX, ii], axis=0)
-	# 			# np.concatenate([cornerPointsY, i], axis=0)
-	# 		del RBox
-
-	thresholdCorner = 0.001*np.amax(R)
-	thresholdEdge = 0.001*np.amin(R)
+	
 	# Based on each pixels value of R, determine if it is a corner or an edge
 	# or neither. 
+	thresholdCorner = np.percentile(R, 95)
 	cornerPoints = np.where(R > thresholdCorner)
-	cornerPointsX = cornerPoints[1]
-	cornerPointsY = cornerPoints[0]
+	# Find local maxima for the corners
+	maxCornerPointsX, maxCornerPointsY = local_maxima(R, cornerPoints, 8)
+
+	thresholdEdge = np.percentile(R, 5)
 	edgePoints = np.where(R < thresholdEdge)
-	edgePointsX = edgePoints[1]
-	edgePointsY = edgePoints[0]
-
-	maxX = len(cornerPointsX)
-	u = np.linspace(0, maxX-1, maxX)
-	# u = np.asarray(u)
-
-	maxCornerPointsX = []
-	maxCornerPointsY = []
-	print(maxX)
-	for i in range(0,maxX):
-		p0x = cornerPointsX[i]*np.ones(maxX)
-		p0y = cornerPointsY[i]*np.ones(maxX)
-		distanceX = (cornerPointsX - p0x)**2
-		distanceY = (cornerPointsY - p0y)**2
-		distance = (distanceX + distanceY)**(1/2)
-		distance = np.delete(distance, i, 0)
-
-		Rmax = 0
-		Xmax = 0
-		Ymax = 0
-		for j in range(8):
-			index = np.where(distance == np.amin(distance))
-			# nearestNeighbour = np.concatenate((nearestNeighbour, index[0]), axis=0)
-			distance[index[0]] = 100000
-			Y = cornerPointsY[index[0]]
-			X = cornerPointsX[index[0]]
-			for k in range(len(Y)):
-				if R[Y[k]][X[k]] > Rmax:
-					Rmax = R[Y[k]][X[k]] 
-					Xmax = X[k]
-					Ymax = Y[k]
-			# print(nearestNeighbour)
-			# print(R[cornerPointsY][cornerPointsX])
-			# nearestNeighbour[1].append(R[cornerPointsY][cornerPointsX])
-		
-		maxCornerPointsX.append(Xmax)
-		maxCornerPointsY.append(Ymax)
-
+	# Find local minima for the edges
+	maxEdgePointsX, maxEdgePointsY = local_maxima(R, edgePoints, 8)
+	
 	# Plot
 	plt.figure()
-	plt.imshow(R, cmap='gray', interpolation='nearest')
+	plt.imshow(intensity, cmap='gray')
+	# plt.imshow(R, cmap='gray', interpolation='nearest')
 	plt.scatter(maxCornerPointsX, maxCornerPointsY, color='r', marker='+')
-	# plt.scatter(edgePointsX, edgePointsY, color='g', marker='+')
+	plt.scatter(maxEdgePointsX, maxEdgePointsY, color='g', marker='+')
 	plt.show()
 
 	return R
 
-def getKey(item):
-	return item[0]
+def local_maxima(R, Points, NN):
+
+	# Function to compute the local maxima of the corners or edges by computing the max among the nearest neighbour of each corner point
+	PointsX = Points[1]
+	PointsY = Points[0]
+	nbPoints = len(PointsX)
+
+	localMaxPointsX = []
+	localMaxPointsY = []
+
+	for i in range(0, nbPoints):
+
+		Point0X = PointsX[i]*np.ones(nbPoints)
+		Point0Y = PointsY[i]*np.ones(nbPoints)
+
+		# Compute the distance between each corner point and cornerPoint0
+		distanceX = (PointsX - Point0X)**2
+		distanceY = (PointsY - Point0Y)**2
+		distance = (distanceX + distanceY)**(1/2)
+		distance = np.delete(distance, i, 0)
+
+		# Looking for the maxima among the cornerPoint0 NN nearest neighbour
+		Rmax = 0
+		Xmax = 0
+		Ymax = 0
+
+		for j in range(NN):
+		
+			index = np.where(distance == np.amin(distance))
+			distance[index[0]] = 100000
+			Y = PointsY[index[0]]
+			X = PointsX[index[0]]
+			for k in range(len(Y)):
+				if abs(R[Y[k]][X[k]]) > Rmax:
+					Rmax = abs(R[Y[k]][X[k]]) 
+					Xmax = X[k]
+					Ymax = Y[k]
+
+		# Save the new corner index		
+		localMaxPointsX.append(Xmax)
+		localMaxPointsY.append(Ymax)
+
+	return localMaxPointsX, localMaxPointsY
 
 # ------------------------- Main Script --------------------------------
 
@@ -197,14 +165,13 @@ HD = (['3.2_1.jpg', '3.2_2.jpg', '3.2_3.jpg',  '4.0_1.jpg', '4.0_2.jpg',
 
 Test_images = (['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg'])
 
-for i in range(0,1):
+for i in range(len(Test_images)):
 
-	intensity, shift = getImageIntensity('dice.jpg')
+	intensity, shift = getImageIntensity(Test_images[i])
 
 	Ix, Iy = derivatives(intensity, shift)
 	
 	GIxx, GIyy, GIxy = gaussian_window(Ix, Iy, 1, shift)
 	
-	R = cornerness_funct(GIxx, GIyy, GIxy, 0.05)
-	# print(np.amax(R))
-	# print(np.amin(R))
+	R = cornerness_funct(intensity, GIxx, GIyy, GIxy, 0.05)
+
